dd7192b3877e47c49165fbaff58929368135137a
jaehong21 <jaehong21@gm.gist.ac.kr>
2023-03-05 00:57:45 +0900

    remove cname

--- platform/module/route53/main.tf
+++ platform/module/route53/main.tf
@@ -3,13 +3,6 @@ resource "aws_route53_zone" "jaehong21" {
   name = "jaehong21.com"
 }
 
-#resource "aws_route53_record" "dev_k3s_route53_record" {
-#  zone_id = aws_route53_zone.doggly_co_kr.id
-#  records = [var.static_ip_address.k3s]
-#  name = "dev.k3s.doggly.co.kr"
-#  type    = "A"
-#  ttl     = "30"
-#}
 #resource "aws_route53_record" "prod_k3s_route53_record" {
 #  zone_id = aws_route53_zone.doggly_co_kr.id
 #  records = [var.static_ip_address.k3s]
