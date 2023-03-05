10b5ed3111a13574435d0e4df9a6003f56ec10fe
jaehong21 <jaehong21@gm.gist.ac.kr>
2023-03-05 09:54:59 +0900

    remove jaehong21 ns transfer to cloudflare

--- platform/module/route53/main.tf
+++ platform/module/route53/main.tf
@@ -1,7 +1,7 @@
 # AWS Route53 Zone
-resource "aws_route53_zone" "jaehong21" {
-  name = "jaehong21.com"
-}
+#  resource "aws_route53_zone" "jaehong21" {
+#    name = "jaehong21.com"
+#  }
 
 #resource "aws_route53_record" "prod_k3s_route53_record" {
 #  zone_id = aws_route53_zone.doggly_co_kr.id
