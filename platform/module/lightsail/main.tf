92eda0fe68b6b525f4f23b3e6b3d25540a293b30
jaehong21 <jaehong21@gm.gist.ac.kr>
2023-03-12 00:50:54 +0900

    new lightsail for docker project

--- platform/module/lightsail/main.tf
+++ platform/module/lightsail/main.tf
@@ -1,38 +1,33 @@
-#  resource "aws_lightsail_instance" "test_instance" {
-#    availability_zone = "ap-northeast-2a"
-#    blueprint_id      = "ubuntu_18_04"
-#    bundle_id         = "nano_2_0"
-#    name              = "test_instance"
-#    key_pair_name = var.key_pair
-#  }
-#  resource "aws_lightsail_instance_public_ports" "test_instance_public_ports" {
-#    instance_name = aws_lightsail_instance.test_instance.name
-#    port_info {
-#      protocol  = "tcp"
-#      from_port = 22
-#      to_port   = 22
-#    }
-#    port_info {
-#      protocol  = "tcp"
-#      from_port = 80
-#      to_port   = 80
-#    }
-#    port_info {
-#      protocol  = "tcp"
-#      from_port = 443
-#      to_port   = 443
-#    }
-#    port_info {
-#      protocol  = "tcp"
-#      from_port = 8080
-#      to_port   = 8080
-#    }
-#  }
-#
-#  resource "aws_lightsail_static_ip" "test_instance_static_ip" {
-#    name = "test_instance_static_ip"
-#  }
-#  resource "aws_lightsail_static_ip_attachment" "test_instance_static_ip_attachment" {
-#    static_ip_name = aws_lightsail_static_ip.test_instance_static_ip.id
-#    instance_name = aws_lightsail_instance.test_instance.id
-#  }
+resource "aws_lightsail_instance" "docker_project" {
+  availability_zone = "ap-northeast-2a"
+  blueprint_id      = "ubuntu_18_04"
+  bundle_id         = "nano_2_0"
+  name              = "docker-project"
+  key_pair_name = var.key_pair
+}
+resource "aws_lightsail_instance_public_ports" "docker_project_public_ports" {
+  instance_name = aws_lightsail_instance.docker_project.name
+  port_info {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+  }
+  port_info {
+    protocol  = "tcp"
+    from_port = 80
+    to_port   = 80
+  }
+  port_info {
+    protocol  = "tcp"
+    from_port = 443
+    to_port   = 443
+  }
+}
+
+resource "aws_lightsail_static_ip" "docker_project_static_ip" {
+  name = "docker-project-static-ip"
+}
+resource "aws_lightsail_static_ip_attachment" "docker_project_static_ip_attachment" {
+  static_ip_name = aws_lightsail_static_ip.docker_project_static_ip.id
+  instance_name = aws_lightsail_instance.docker_project.id
+}
