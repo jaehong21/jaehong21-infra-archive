3a41d9b8f2346e28fa65e604871fb1c0244a5417
jaehong21 <jaehong21@gm.gist.ac.kr>
2023-04-05 17:05:01 +0900

    destroy lightsail

--- platform/main.tf
+++ platform/main.tf
@@ -1,6 +1,7 @@
 module "key_pair" {
   source = "./module/key-pair"
   dev_key_path = "/Users/jaehong21/Desktop/server-key/dev_key.pub"
+  jaehong21_key_path = "/Users/jaehong21/Desktop/server-key/jaehong21_key.pub"
 }
 
 module "route53" {
@@ -9,7 +10,7 @@ module "route53" {
 
 module "lightsail" {
   source = "./module/lightsail"
-  key_pair = module.key_pair.dev_key_pair_lightsail
+  key_pair = module.key_pair.jaehong21_key_pair_lightsail
 }
 #  output "lightsail" {
 #    value = module.lightsail.test_instance_static_ip
