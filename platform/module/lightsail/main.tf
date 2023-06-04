7dfa030f69b927d3292a03285dddefbbf954c23a
jaehong21 <jaehong21@gm.gist.ac.kr>
2023-06-04 13:07:46 +0900

    ✨ feat: set Outline VPN

--- platform/module/lightsail/main.tf
+++ platform/module/lightsail/main.tf
@@ -3,29 +3,34 @@ resource "aws_lightsail_instance" "jaehong21_vpn" {
   blueprint_id      = "ubuntu_20_04"
   bundle_id         = "nano_2_0"
   name              = "jaehong21-vpn"
-  key_pair_name = var.key_pair
+  key_pair_name     = var.key_pair
 }
 resource "aws_lightsail_instance_public_ports" "jaehong21_vpn_public_ports" {
   instance_name = aws_lightsail_instance.jaehong21_vpn.name
   port_info {
-    protocol  = "tcp"
     from_port = 22
     to_port   = 22
+    protocol  = "tcp"
   }
   port_info {
-    protocol  = "tcp"
-    from_port = 80
-    to_port   = 80
-  }
-  port_info {
-    protocol  = "tcp"
-    from_port = 443
-    to_port   = 443
-  }
-    port_info {
-    protocol  = "tcp"
     from_port = 8080
     to_port   = 8080
+    protocol  = "tcp"
+  }
+  port_info {
+    from_port = 24148
+    to_port   = 24148
+    protocol  = "tcp"
+  }
+  port_info {
+    from_port = 58401
+    to_port   = 58401
+    protocol  = "tcp"
+  }
+  port_info {
+    from_port = 58401
+    to_port   = 58401
+    protocol  = "udp"
   }
 }
 
@@ -34,5 +39,5 @@ resource "aws_lightsail_static_ip" "jaehong21_vpn_static_ip" {
 }
 resource "aws_lightsail_static_ip_attachment" "jaehong21_static_ip_attachment" {
   static_ip_name = aws_lightsail_static_ip.jaehong21_vpn_static_ip.name
-  instance_name = aws_lightsail_instance.jaehong21_vpn.name
+  instance_name  = aws_lightsail_instance.jaehong21_vpn.name
 }
